1a33c57824fe460febe274506190902231fb1a73
Shiteng Gao <shitengg@uci.edu>
2019-10-28 12:55:07 -0700

    Intorduce React to the server

---
+++ source/LetsEat/app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var apiRouter = require('./routes/api');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.use('/api', apiRouter);
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
+
+module.exports = app;
