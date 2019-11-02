e2b2f0a5e955d01880c840172ffe98b88078dfe8
Sunnyhith Kanakanti <skanakan@uci.edu>
2019-11-02 00:33:55 -0700

    Updating Survey.js

--- source/LetsEat/client/src/components/preferences/Survey.js
+++ source/LetsEat/client/src/components/preferences/Survey.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export class Survey extends Component {
+class Survey extends Component {
   state = {
     firstName: "",
     lastName: "",
@@ -9,27 +9,61 @@ export class Survey extends Component {
     pricePreference: "",
     favCuisines: []
   };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
   render() {
     return (
-      <div className="card-panel">
-        <h4 className="header2">This is the Survey Page</h4>
-        <div className="row">
-          <form className="col s12 l5" _lpchecked="1">
-            <div className="row">
-              <div className="input-field col s12 l5">
-                <input placeholder="John Doe" type="text" />
-                <label for="first_name" class="active">
-                  Name
-                </label>
-              </div>
-            </div>
-            <div className="row"></div>
-            <div className="row"></div>
-            <div className="row"></div>
-            <div className="row"></div>
-            <div className="row"></div>
-          </form>
-        </div>
+      <div className="container">
+        <form onSubmit={this.handleSubmit} className="white">
+          <h4 className="grey-text text-darken-3">
+            Please fill out the following details
+          </h4>
+          <div className="input-field">
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" id="firstName" onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text" id="lastName" onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="currentLocation">Current Location</label>
+            <input
+              type="text"
+              id="currentLocation"
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="input-field">
+            <label htmlFor="foodPreferences">Food Preferences</label>
+            <input
+              type="text"
+              id="foodPreferences"
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="input-field">
+            <label htmlFor="pricePreference">Price Preference</label>
+            <input
+              type="text"
+              id="pricePreference"
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="input-field">
+            <button className="btn red z-depth-0">Submit</button>
+          </div>
+        </form>
       </div>
     );
   }
